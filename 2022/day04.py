0393b094a1bb7423d925bba115ea90ef73891558
Mike Roufa <roufamatic@gmail.com>
2022-12-03 22:24:48 -0800

    AoC 2022 day 4!

--- 2022/day04.py
+++ 2022/day04.py
@@ -12,6 +12,27 @@ def ReadFile(path):
 def Run(path):
   input = ReadFile(path)
   lines = [v.strip() for v in input.split('\n')]
+  total = 0
+  for line in lines:
+    (r1, r2) = line.split(',')
+    (l1, h1) = [int(v) for v in r1.split('-')]
+    (l2, h2) = [int(v) for v in r2.split('-')]
+    if (l1 <= l2 and h1 >= h2) or (l2 <= l1 and h2 >= h1):
+      total += 1
+  print(total)
+
+  total = 0
+  for line in lines:
+    (r1, r2) = line.split(',')
+    (l1, h1) = [int(v) for v in r1.split('-')]
+    (l2, h2) = [int(v) for v in r2.split('-')]
+    
+    rr1 = set(range(l1, h1 + 1))
+    rr2 = set(range(l2, h2 + 1))
+    if len(rr1.intersection(rr2)) > 0:
+      total += 1
+    
+  print(total)
 
 
 Run("input04t.txt")
